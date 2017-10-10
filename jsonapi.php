1e6c07dbb172cf2c971006cde94e50f704fbf030
dewalay101 <32671326+dewalay101@users.noreply.github.com>
2017-10-10 13:10:02 +0100

    Update jsonapi.php

--- jsonapi.php
+++ jsonapi.php
@@ -2,7 +2,7 @@
 
 $item=array();
 for($i= 0; $i<10; $i++) {
-$item[$i] = "This is message $i";
+    $item[$i] = "This is message $i";
 }
 header('Content-Type: application/json');
 $jsonOutput = json_encode($items);
